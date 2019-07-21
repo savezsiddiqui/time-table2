e175fa9629fc3faf2beeeb3f45271ef4f7a1af5e
Savez Siddiqui <savezsiddiqui@gmail.com>
2019-07-21 13:31:39 +0530

    getting started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Main from './components/Main'
+import schedule from './data/myclasses.json'
+import myclasses from './data/classes.json'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Main
+      schedule={schedule}
+      myclasses={myclasses}
+    />
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { Container, Row, ButtonGroup, Button, Col } from 'react-bootstrap'
+
+const Card = ({ subject, location, time }) => {
+    return (
+        <Col xs={12}>
+            <p className='mb-0'>{subject}</p>
+            <p className='mt-0'>{time} | {location}</p>
+        </Col>
+    )
+}
+
+class Main extends Component {
+
+    state = {
+        index: 0,
+    }
+
+    onClickLeft() {
+        this.setState({
+            index: this.state.index === 0 ? 5 : this.state.index - 1
+        })
+    }
+
+    onClickRight() {
+        this.setState({
+            index: this.state.index === 5 ? 0 : this.state.index + 1
+        })
+    }
+
+    render() {
+
+        const { schedule, myclasses } = this.props
+        const item = schedule[this.state.index]
+        let RenderData = []
+
+        Object.keys(item)
+            .forEach((key, index) => {
+                if (index > 0 && item[key].length > 0) {
+
+                    let str = item[key][0]
+                    let time = key
+                    let subject = myclasses[str.slice(str.indexOf('(') + 1,
+                        str.indexOf(')'))]
+                    let location = str.slice(str.indexOf(')') + 2, str.indexOf('/'))
+
+                    RenderData.push({ time, subject, location })
+                }
+            })
+
+        const CardRender = RenderData.map(item => {
+            return <Card
+                subject={item.subject}
+                location={item.location}
+                time={item.time} />
+        })
+
+        return (
+            <Container className='text-center'>
+                <Row>
+                    <Col xs={12}>
+                        <h2>{schedule[this.state.index].day}</h2>
+                    </Col>
+                    {CardRender}
+                    <Col xs={12}>
+                        <ButtonGroup>
+                            <Button variant="secondary"
+                                onClick={() => this.onClickLeft()}
+                            >Left</Button>
+                            <Button variant="secondary"
+                                onClick={() => this.onClickRight()}
+                            >Right</Button>
+                        </ButtonGroup>
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Main
