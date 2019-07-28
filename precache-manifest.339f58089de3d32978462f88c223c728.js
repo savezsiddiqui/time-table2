dce12d342ff284ec694a7af23d043977ca799dd8
Savez Siddiqui <savezsiddiqui@gmail.com>
2019-07-28 12:34:58 +0530

    Updates

---
+++ precache-manifest.339f58089de3d32978462f88c223c728.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3309fafa6048c27451df2353dd26cb2c",
+    "url": "/time-table2/index.html"
+  },
+  {
+    "revision": "0d18b7f40a155f1e4ee5",
+    "url": "/time-table2/static/css/main.772b6dd6.chunk.css"
+  },
+  {
+    "revision": "9c4e2da7d6575156ddb8",
+    "url": "/time-table2/static/js/2.dac25531.chunk.js"
+  },
+  {
+    "revision": "0d18b7f40a155f1e4ee5",
+    "url": "/time-table2/static/js/main.9a2ce28f.chunk.js"
+  },
+  {
+    "revision": "6e3a7c67ff7b6f9e276f",
+    "url": "/time-table2/static/js/runtime~main.58b463d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time-table2/precache-manifest.498cad82f852895cf64ee90778fa8379.js"
+  "/time-table2/precache-manifest.339f58089de3d32978462f88c223c728.js"
 );
 
 self.addEventListener('message', (event) => {
